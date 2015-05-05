6c26ae4c6decd3c47195bf0357e9651c5672df8c
EverCedillo <EverCedillo@users.noreply.github.com>
2015-05-05 16:30:13 -0500

    Add fadein mask, nav bar

--- static/script/scripts.js
+++ static/script/scripts.js
@@ -8,11 +8,8 @@ function init(){
 function fadeinShow(){
 	cont=0;
 	banElements= document.getElementsByClassName('fadeInShow');
-	console.log(banElements);
-	console.log('ho');
 	$(banElements[cont]).toggleClass('visible');
 	window.setInterval(function(){
-		console.log(cont);
 		$(banElements[cont]).toggleClass('visible');
 		cont=cont==banElements.length-1?0:cont+1;
 		$(banElements[cont]).toggleClass('visible');
