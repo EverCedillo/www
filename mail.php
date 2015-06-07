4697f4e1368b4660f016dbf5dc0117549052ae26
EverCedillo <EverCedillo@users.noreply.github.com>
2015-06-06 23:04:51 -0500

    Test Responsive, agregado logo

--- mail.php
+++ mail.php
@@ -1,2 +1,25 @@
-mail.php
-$sql = "INSERT INTO `ohana_mailing`.`TOH001_MAIL_LIST` (`CD_INTERESADO`, `NB_MAIL`, `TM_STAMP`) VALUES (NULL, \'\', CURRENT_TIMESTAMP);";
+<html>
+<body>
+ 
+ 
+<?php
+$con = mysql_connect("ohana_mailing.localhost","web","ohana_psw");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+ 
+mysql_select_db("web", $con);
+$sql = "INSERT INTO `ohana_mailing`.`TOH001_MAIL_LIST` (,`NB_MAIL`, `TM_STAMP`) VALUES ('$_POST[email]', CURRENT_TIMESTAMP);";
+
+ 
+if (!mysql_query($sql,$con))
+  {
+  die('Error: ' . mysql_error());
+  }
+echo "1 record added";
+ 
+mysql_close($con)
+?>
+</body>
+</html>
--- static/script/scripts.js
+++ static/script/scripts.js
@@ -1,11 +1,7 @@
 $( document ).ready(init);
 
 function init(){
-	/*$(window).on('beforeunload', function() {
-		$('#Modal').toggleClass('visible');
-    	return 'Your own message goes here...';
-	});*
-	fadeinShow();*/
+
 	$('#RegContainer').click(function(event) {
 		event.stopPropagation();
 	})
@@ -42,40 +38,3 @@ $(window).scroll(function(event) {
 		}
 	}
 })
-
-function loadTimeLine(callback){
-	jQuery.ajax({
-		url:"nosotros.html",
-		cache:false,
-		dataType: "text",
-		async:true,
-		success: function (text) {
-			callback(text);
-		}
-
-	});
-}
-function sendTimeLine(content) {
-	$(coso).html(content);
-}
-
-
-/*
-function bye(){
-	alert("bye");
-	console.log("bye")
-}
-
-function fadeinShow(){
-	cont=0;
-	banElements= document.getElementsByClassName('fadeInShow');
-	$(banElements[cont]).toggleClass('visible');
-	window.setInterval(function(){
-		$(banElements[cont]).toggleClass('visible');
-		cont=cont==banElements.length-1?0:cont+1;
-		$(banElements[cont]).toggleClass('visible');
-	}
-	,3000);
-}
-
-*/
