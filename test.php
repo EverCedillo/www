7d2d07ec450c2331b1b14fd3bb06a847e05e270d
EverCedillo <EverCedillo@users.noreply.github.com>
2015-06-13 00:35:03 -0500

    Test Android-JSON

---
+++ test.php
@@ -0,0 +1,22 @@
+<?php
+	$data = [  
+   {  
+      "title":"Leu00f3n",
+      "data":"El leu00f3n (Panthera leo) es un mamu00edfero carnu00edvoro ...",
+      "foot":"leon"
+   },
+   {  
+      "title":"Elefante",
+      "data":"Los elefantes o elefu00e1ntidos (Elephantidae) son ...",
+      "foot":"elefante"
+   },
+   {  
+      "title":"Cocodrilo",
+      "data":"Los crocodu00edlidos (Crocodylidae) son una familia de sauru00f3psidos ...",
+      "foot":"cocodrilo"
+   }
+];
+
+header('Content-Type: application/json');
+echo $data;
+?>
