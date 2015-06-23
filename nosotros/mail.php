ec0fe6a5f95176a8b357c496e1a2c085262916f3
EverCedillo <EverCedillo@users.noreply.github.com>
2015-06-22 23:54:41 -0500

    padding bottom slide-title

---
+++ nosotros/mail.php
@@ -0,0 +1,26 @@
+<html>
+<body>
+ 
+ 
+<?php
+$con = mysql_connect("localhost","web","ohana_psw");
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+ 
+mysql_select_db("web", $con);
+$sql = "INSERT INTO ohana_mailing.TOH001_MAIL_LIST ('NB_MAIL') VALUES ('$_POST[email_ohana]')";
+$sql = "INSERT INTO `ohana_mailing`.`TOH001_MAIL_LIST` (`CD_INTERESADO`, `NB_MAIL`, `TM_STAMP`) VALUES (NULL, '$_POST[email_ohana]', CURRENT_TIMESTAMP);";
+ 
+if (!mysql_query($sql,$con))
+  {
+  die('Error: ' . mysql_error());
+  }
+	echo "1 record added";
+  
+ 
+mysql_close($con)
+?>
+</body>
+</html>
