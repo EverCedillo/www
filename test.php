e3daadea35183cdcb20c30702c799f35bbf3a2a1
Ever <ever.cedillo@outlook.com>
2016-01-04 23:20:56 -0600

    Test scale

--- test.php
+++ test.php
@@ -1,5 +1,11 @@
 <?php
 
+require 'vendor/autoload.php';
+ 
+use Parse\ParseClient;
+ 
+ParseClient::initialize('Gjy7cLCCl7HcIsLS6myQDIxUvYSdz2ZCVNGHpd8G', 'dtMnemfa8ZLomhY6ngmcEC3MQeFwao0z3HBlbKor', 'ONl2Nnb4QAfcFIZNOXrzgMeyzBnuhio63TIjqMWV');
+
 $holi=$_GET["string"];
 	$data = '[{  
       "title":"León",
