0da0d6a143904045117fdebaed01d5596f66bf2d
Ever <ever.cedillo@outlook.com>
2016-01-04 23:39:48 -0600

    Test scale

--- list.php
+++ list.php
@@ -1,21 +1,20 @@
 <?php
-
 require 'vendor/autoload.php';
  
 use Parse\ParseClient;
  
 ParseClient::initialize('Gjy7cLCCl7HcIsLS6myQDIxUvYSdz2ZCVNGHpd8G', 'dtMnemfa8ZLomhY6ngmcEC3MQeFwao0z3HBlbKor', 'ONl2Nnb4QAfcFIZNOXrzgMeyzBnuhio63TIjqMWV');
 
-$holi=$_GET["data"];
 
-echo $data;
-echo $holi;
+$query = new ParseQuery("TestScale");
 
-use Parse\ParseObject;
- 
-$testObject = ParseObject::create("TestScale");
-$testObject->set("data", $holi);
-$testObject->save();
+$query->exists("data");
 
-echo $testObject;
+foreach ($query as $key) {
+	# code...
+	echo $key;
+	echo "\n";
+}
+
+
 ?>
