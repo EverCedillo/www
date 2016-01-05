3e23d6ffcaa9b762f8ec71a35eb6f481d4c14c23
Ever <ever.cedillo@outlook.com>
2016-01-04 23:58:03 -0600

    Test scale

--- list.php
+++ list.php
@@ -1,6 +1,20 @@
 <?php
+require 'vendor/autoload.php';
+ 
+use Parse\ParseClient;
+use Parse\ParseObject;
+ 
+ParseClient::initialize('Gjy7cLCCl7HcIsLS6myQDIxUvYSdz2ZCVNGHpd8G', 'dtMnemfa8ZLomhY6ngmcEC3MQeFwao0z3HBlbKor', 'ONl2Nnb4QAfcFIZNOXrzgMeyzBnuhio63TIjqMWV');
 
-echo "string";
+
+$query = new ParseQuery("TestScale");
+
+$query->exists("data");
+
+for ($i = 0; $i < count($results); $i++) {
+  $object = $results[$i];
+  echo $object->getObjectId() . ' - ' . $object->get('playerName');
+}
 
 
 ?>
