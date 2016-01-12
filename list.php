a62e50aa2dfef4363d5539b3ff7320421c7d9ec2
Ever <ever.cedillo@outlook.com>
2016-01-11 21:33:40 -0600

    Update TestScale

--- list.php
+++ list.php
@@ -12,7 +12,7 @@ ParseClient::initialize('Gjy7cLCCl7HcIsLS6myQDIxUvYSdz2ZCVNGHpd8G', 'dtMnemfa8ZL
 
 $query = new ParseQuery("TestScale");
 
-$query->select("data");
+$query->exists("data");
 $query->descending("updatedAt");
 
 $results = $query->find();
