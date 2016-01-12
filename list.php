efc15bbe0f46bdb44dd4aade073238fb704a1e16
Ever <ever.cedillo@outlook.com>
2016-01-11 22:19:09 -0600

    Update TestScale

--- list.php
+++ list.php
@@ -1,13 +1,11 @@
-<list></list>
 <?php
 require 'vendor/autoload.php';
 
 header("refresh: 10;");
  
-
-use Parse\*;
-
-
+use Parse\ParseClient;
+use Parse\ParseObject;
+use Parse\ParseQuery;
  
 ParseClient::initialize('Gjy7cLCCl7HcIsLS6myQDIxUvYSdz2ZCVNGHpd8G', 'dtMnemfa8ZLomhY6ngmcEC3MQeFwao0z3HBlbKor', 'ONl2Nnb4QAfcFIZNOXrzgMeyzBnuhio63TIjqMWV');
 
@@ -35,27 +33,4 @@ for ($i = 0; $i < count($results); $i++) {
 
 ?>
 
-<script src="//www.parsecdn.com/js/parse-1.6.14.min.js"></script>
-<script type="text/javascript" charset="utf-8" async defer>
-	
-	Parse.initialize("Gjy7cLCCl7HcIsLS6myQDIxUvYSdz2ZCVNGHpd8G", "5z9QFpnWx09U1aznCU5z38KRa53tFufHiGAXZwVO");
-	var TestScale = Parse.Object.extend("TestScale");
-	var query = new Parse.Query(TestScale);
-	query.exists("data");
-	query.find({
-  		success: function(results) {
-    	var content="//<br>";
-    	// Do something with the returned Parse.Object values
-    	for (var i = 0; i < results.length; i++) {
-      		var object = results[i];
-      	
-      		content=content+object.id + ' - ' + object.get('data') + ' - ' object.updatedAt + '<br>';
-    	}
-    	document.getElementsByTagName('list')[0].innerHTML=content;
-  },
-  error: function(error) {
-    
-  }
-});
-</script>
 
