2db75fe1daf1d8b559e540c96b20f2398a3b7872
Ever <ever.cedillo@outlook.com>
2016-01-11 22:43:42 -0600

    Update TestScale

--- list.php
+++ list.php
@@ -48,7 +48,7 @@ for ($i = 0; $i < count($results); $i++) {
     	for (var i = 0; i < results.length; i++) {
       		var object = results[i];
       	
-      		content=content+object.id + ' - ' + object.get('data') + ' - ' object.createdAt + '<br>';
+      		content=content+object.id + ' - ' + object.get('data') + ' - ' object.get('updatedAt') + '<br>';
     	}
     	document.getElementsByTagName('list')[0].innerHTML=content;
   },
