660362b8cff7774199a6bdc68bd67c62debc7189
Ever <ever.cedillo@outlook.com>
2016-05-06 23:56:14 -0500

    Upload images

--- upload.php
+++ upload.php
@@ -3,8 +3,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$path = $_GET["file"];
-$pic = $_GET["pic"];
+$path = $_POST["file"];
+$pic = $_POS["pic"];
 
 echo base64_to_jpeg($pic,$path);
 
@@ -24,5 +24,3 @@ function base64_to_jpeg( $base64_string, $output_file ) {
 }
 
 ?>
-
-iVBORw0KGgoAAAANSUhEUgAAABwAAAASCAMAAAB/2U7WAAAABlBMVEUAAAD///+l2Z/dAAAASUlEQVR4XqWQUQoAIAxC2/0vXZDrEX4IJTRkb7lobNUStXsB0jIXIAMSsQnWlsV+wULF4Avk9fLq2r8a5HSE35Q3eO2XP1A1wQkZSgETvDtKdQAAAABJRU5ErkJggg==
