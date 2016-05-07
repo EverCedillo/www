372509efc2dcd8833e1259723cd3eba30859e23f
Ever <ever.cedillo@outlook.com>
2016-05-06 22:40:09 -0500

    Upload images

---
+++ upload.php
@@ -0,0 +1,19 @@
+<?php
+
+$path = $_GET["file"];
+$pic = $_GET["pic"];
+
+base64_to_jpeg($pic,$path);
+
+echo $path;
+
+
+function base64_to_jpeg( $base64_string, $output_file ) {
+    //$ifp = fopen( $output_file, "wb" ); 
+    //fwrite( $ifp, base64_decode( $base64_string) ); 
+    //fclose( $ifp ); 
+    echo "string" + $base64_string;
+    return( $output_file ); 
+}
+
+?>
