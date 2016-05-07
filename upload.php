456be676db4955ef17a061f19f3e062d4f72f04c
Ever <ever.cedillo@outlook.com>
2016-05-06 23:09:17 -0500

    Upload images

--- upload.php
+++ upload.php
@@ -9,11 +9,17 @@ echo $path;
 
 
 function base64_to_jpeg( $base64_string, $output_file ) {
-    $ifp = fopen( "/home/ever/file.png", "wb" ); 
-    fwrite( $ifp, base64_decode( $base64_string) ); 
-    fclose( $ifp ); 
-    echo  $base64_string;
+    $ifp = fopen( "/home/ever/file.png", "wb" );
+    if($ifp==false) {
+    	echo "Fallé";
+    }else{
+	    fwrite( $ifp, base64_decode( $base64_string) ); 
+	    fclose( $ifp ); 
+	    echo  $base64_string;
+	}
     return( $output_file ); 
 }
 
 ?>
+
+iVBORw0KGgoAAAANSUhEUgAAABwAAAASCAMAAAB/2U7WAAAABlBMVEUAAAD///+l2Z/dAAAASUlEQVR4XqWQUQoAIAxC2/0vXZDrEX4IJTRkb7lobNUStXsB0jIXIAMSsQnWlsV+wULF4Avk9fLq2r8a5HSE35Q3eO2XP1A1wQkZSgETvDtKdQAAAABJRU5ErkJggg==
